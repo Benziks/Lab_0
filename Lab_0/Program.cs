0d80a29cee210fb5c30dc7f9c7e22022a5a394f8
Benziks <joker.nytik@gmail.com>
2023-09-12 13:39:37 +0300

    Update

--- Lab_0/Program.cs
+++ Lab_0/Program.cs
@@ -6,19 +6,22 @@ using System.Threading.Tasks;
 
 namespace Lab0
 {
+    
     internal class Program
     {
+        static int Multiplication(int FirstValue, int SecondValues)
+        {
+            return FirstValue * SecondValues;
+        }
         static void Main(string[] args)
         {
             Random random = new Random();
             int FirstValue = random.Next(100 - 50);
             int SecondValue = random.Next(100 - 50);
-            int result;
-            result = FirstValue * SecondValue;
-            Console.WriteLine($"\n Ваше первое число:{FirstValue} \nВаше второе число:{SecondValue} \nРезультат вычислений: {result}");
+            Console.WriteLine($"Ваше первое число:{FirstValue} \nВаше второе число:{SecondValue} \nРезультат вычислений: {Multiplication(FirstValue,SecondValue)}");
 
 
-
+            
 
             Console.ReadLine();
 
